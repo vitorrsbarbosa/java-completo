bc3d14d20a2533b2d877054d1f3ca4ebe5a8403d
Vitor Barbosa <vitorricardosilvabarbosa@gmail.com>
2024-09-24 12:55:12 -0300

    Chess Game: * Implementing defensive programming using Exceptions

--- src/exercicio09/board/Board.java
+++ src/exercicio09/board/Board.java
@@ -1,5 +1,7 @@
 package exercicio09.board;
 
+import exercicio09.board.exceptions.BoardException;
+
 public class Board {
 	private int rows;
 	private int columns;
@@ -11,6 +13,9 @@ public class Board {
 	}
 
 	public Board( int rows, int columns ) {
+		if( rows < 1 || columns < 1 ) {
+			throw new BoardException( "Error creating board: There must be at least 1 row and 1 column." );
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[ rows ][ columns ];
@@ -20,36 +25,51 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows( int rows ) {
-		this.rows = rows;
-	}
-
 	public int getColumns( ) {
 		return columns;
 	}
 
-	public void setColumns( int columns ) {
-		this.columns = columns;
-	}
-
 	public Piece piece( int row, int column ) {
+		if( ! positionExists( row, column ) ) {
+			throw new BoardException( "Position not on the board." );
+		}
 		return pieces[ row ][ column ];
 	}
 
 	public Piece piece( Position position ) {
+		if( ! positionExists( position ) ) {
+			throw new BoardException( "Position not on the board." );
+		}
 		return pieces[ position.getRow( ) ][ position.getColumn( ) ];
 	}
 
 	public void placePiece( Piece piece, Position position ) {
+		if( thereIsAPiece( position ) ) {
+			throw new BoardException( "There is already a piece on " + position );
+		}
 		pieces[ position.getRow( ) ][ position.getColumn( ) ] = piece;
 		piece.setPosition( position );
 	}
 
 	public Piece removePiece( Position position ) {
+		if( ! thereIsAPiece( position ) ) {
+			throw new BoardException( "There is no piece on " + position );
+		}
 		return pieces[ position.getRow( ) ][ position.getColumn( ) ];
 	}
 
+	private boolean positionExists( int row, int column ) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
 	public boolean positionExists( Position position ) {
-		return position.getRow( ) <= rows && position.getColumn( ) <= columns;
+		return positionExists( position.getRow( ), position.getColumn( ) );
+	}
+
+	public boolean thereIsAPiece( Position position ) {
+		if( ! positionExists( position ) ) {
+			throw new BoardException( "Position not on the board." );
+		}
+		return piece( position ) != null;
 	}
 }
---
+++ src/exercicio09/board/exceptions/BoardException.java
@@ -0,0 +1,7 @@
+package exercicio09.board.exceptions;
+
+public class BoardException extends RuntimeException {
+	public BoardException( String message ) {
+		super( message );
+	}
+}
