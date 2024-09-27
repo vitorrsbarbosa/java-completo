93675429fc2643fb1e24d1d1a12c02ad3552ac5a
Vitor <vitorricardosilvabarbosa@gmail.com>
2024-09-27 15:21:52 -0300

    Chess game * Implements Rook's movement and possible moves

--- src/exercicio09/chess/pieces/entities/Rook.java
+++ src/exercicio09/chess/pieces/entities/Rook.java
@@ -19,79 +19,83 @@ public class Rook extends ChessPiece {
 	public boolean[][] possibleMoves( ) {
 		boolean[][] mat = new boolean[ getBoard( ).getRows( ) ][ getBoard( ).getColumns( ) ];
 		Position p = new Position( 0, 0 );
-		toRight( p, mat );
-		toBellow( p, mat );
-		toLeft( p, mat );
-		toAbove( p, mat );
+		toNorth( p, mat );
+		toEast( p, mat );
+		toSouth( p, mat );
+		toWest( p, mat );
 
 		return mat;
 	}
 
 	@Override
-	public void toRight( Position p, boolean[][] mat ) {
-		// right
-		p.setValues( position.getRow( ), position.getColumn( ) + 1 );
-		while( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
-			p.setColumn( p.getColumn( ) + 1 );
+	public void toNorth( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ) - 1, position.getColumn( ) );
+		while( getBoard( ).positionExists( newPosition ) && ! getBoard( ).thereIsAPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
+			newPosition.setRow( newPosition.getRow( ) - 1 );
 		}
-		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
-		}
-	}
-
-	@Override
-	public void toDiagonalRightBellow( Position p, boolean[][] mat ) {
-	}
-
-	@Override
-	public void toBellow( Position p, boolean[][] mat ) {
-		// bellow
-		p.setValues( position.getRow( ) + 1, position.getColumn( ) );
-		while( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
-			p.setRow( p.getRow( ) + 1 );
-		}
-		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		if( getBoard( ).positionExists( newPosition ) && isThereOpponentPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
 		}
 	}
 
 	@Override
-	public void toDiagonalLeftBellow( Position p, boolean[][] mat ) {
+	public void toNorthEast( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ), position.getColumn( ) );
+		mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = false;
 	}
 
 	@Override
-	public void toLeft( Position p, boolean[][] mat ) {
-		// left
-		p.setValues( position.getRow( ), position.getColumn( ) - 1 );
-		while( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
-			p.setColumn( p.getColumn( ) - 1 );
+	public void toEast( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ), position.getColumn( ) - 1 );
+		while( getBoard( ).positionExists( newPosition ) && ! getBoard( ).thereIsAPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
+			newPosition.setColumn( newPosition.getColumn( ) - 1 );
 		}
-		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
-		}
-	}
-
-	@Override
-	public void toDiagonalLeftAbove( Position p, boolean[][] mat ) {
-	}
-
-	@Override
-	public void toAbove( Position p, boolean[][] mat ) {
-		// above
-		p.setValues( position.getRow( ) - 1, position.getColumn( ) );
-		while( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
-			p.setRow( p.getRow( ) - 1 );
-		}
-		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
-			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		if( getBoard( ).positionExists( newPosition ) && isThereOpponentPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
 		}
 	}
 
 	@Override
-	public void toDiagonalRightAbove( Position p, boolean[][] mat ) {
+	public void toSouthEast( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ), position.getColumn( ) );
+		mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = false;
+	}
+
+	@Override
+	public void toSouth( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ) + 1, position.getColumn( ) );
+		while( getBoard( ).positionExists( newPosition ) && ! getBoard( ).thereIsAPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
+			newPosition.setRow( newPosition.getRow( ) + 1 );
+		}
+		if( getBoard( ).positionExists( newPosition ) && isThereOpponentPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
+		}
+	}
+
+	@Override
+	public void toSouthWest( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ), position.getColumn( ) );
+		mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = false;
+	}
+
+	@Override
+	public void toWest( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ), position.getColumn( ) + 1 );
+		while( getBoard( ).positionExists( newPosition ) && ! getBoard( ).thereIsAPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
+			newPosition.setColumn( newPosition.getColumn( ) + 1 );
+		}
+		if( getBoard( ).positionExists( newPosition ) && isThereOpponentPiece( newPosition ) ) {
+			mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = true;
+		}
+	}
+
+	@Override
+	public void toNorthWest( Position newPosition, boolean[][] mat ) {
+		newPosition.setValues( position.getRow( ), position.getColumn( ) );
+		mat[ newPosition.getRow( ) ][ newPosition.getColumn( ) ] = false;
 	}
 }
