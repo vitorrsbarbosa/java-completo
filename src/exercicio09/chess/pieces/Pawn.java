c667c08e6eaffdb608692f88c47e3d7291e27606
Vitor <vitorricardosilvabarbosa@gmail.com>
2024-09-30 16:49:53 -0300

    Chess game * Logic implementation of Pawn movements

--- src/exercicio09/chess/pieces/Pawn.java
+++ src/exercicio09/chess/pieces/Pawn.java
@@ -14,15 +14,17 @@ public class Pawn extends ChessPiece {
 	public boolean[][] possibleMoves( ) {
 		boolean[][] mat = new boolean[ getBoard( ).getRows( ) ][ getBoard( ).getColumns( ) ];
 		Position p = new Position( 0, 0 );
-		toNorth( p, mat );
-		// toNorthEast( p,mat );
-		toEast( p, mat );
-		// toSouthEast( p,mat );
-		toSouth( p, mat );
-		// toSouthWest( p,mat );
-		toWest( p, mat );
-		// toNorthWest( p,mat );
 
+		if( getColor( ).equals( Color.WHITE ) ) {
+			toNorth( p, mat );
+			toNorthWest( p, mat );
+			toNorthEast( p, mat );
+		}
+		if( getColor( ).equals( Color.BLACK ) ) {
+			toSouth( p, mat );
+			toSouthWest( p, mat );
+			toSouthEast( p, mat );
+		}
 		return mat;
 	}
 
@@ -33,44 +35,69 @@ public class Pawn extends ChessPiece {
 
 	@Override
 	public void toNorth( Position p, boolean[][] mat ) {
-
+		p.setValues( position.getRow( ) - 1, position.getColumn( ) );
+		if( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		}
+		p.setValues( position.getRow( ) - 2, position.getColumn( ) );
+		Position p2 = new Position( position.getRow( ) - 1, position.getColumn( ) );
+		if( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) && getBoard( ).positionExists( p2 ) && ! getBoard( ).thereIsAPiece( p2 ) && getMoveCount( ) == 0 ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		}
 	}
 
 	@Override
 	public void toNorthEast( Position p, boolean[][] mat ) {
+		p.setValues( position.getRow( ) - 1, position.getColumn( ) + 1 );
+		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		}
 	}
 
 	@Override
 	public void toEast( Position p, boolean[][] mat ) {
-		mat[ position.getRow( ) ][ position.getColumn( ) ] = false;
+		mat[ p.getRow( ) ][ p.getColumn( ) ] = false;
 	}
 
 	@Override
 	public void toSouthEast( Position p, boolean[][] mat ) {
-
+		p.setValues( position.getRow( ) + 1, position.getColumn( ) + 1 );
+		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		}
 	}
 
 	@Override
 	public void toSouth( Position p, boolean[][] mat ) {
-
+		p.setValues( position.getRow( ) + 1, position.getColumn( ) );
+		if( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		}
+		p.setValues( position.getRow( ) + 2, position.getColumn( ) );
+		Position p2 = new Position( position.getRow( ) + 1, position.getColumn( ) );
+		if( getBoard( ).positionExists( p ) && ! getBoard( ).thereIsAPiece( p ) && getBoard( ).positionExists( p2 ) && ! getBoard( ).thereIsAPiece( p2 ) && getMoveCount( ) == 0 ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		}
 	}
 
 	@Override
 	public void toSouthWest( Position p, boolean[][] mat ) {
-		p.setValues( position.getRow( ) + 1, position.getColumn( ) + 1 );
-		while( getBoard( ).positionExists( position ) && getBoard( ).thereIsAPiece( position ) && isThereOpponentPiece( position ) ) {
-			mat[ position.getRow( ) ][ position.getColumn( ) ] = true;
-			p.setValues( position.getRow( ) + 1, position.getColumn( ) + 1 );
+		p.setValues( position.getRow( ) + 1, position.getColumn( ) - 1 );
+		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
 		}
 	}
 
 	@Override
 	public void toWest( Position p, boolean[][] mat ) {
-		mat[ position.getRow( ) ][ position.getColumn( ) ] = false;
+		mat[ p.getRow( ) ][ p.getColumn( ) ] = false;
 	}
 
 	@Override
 	public void toNorthWest( Position p, boolean[][] mat ) {
-
+		p.setValues( position.getRow( ) - 1, position.getColumn( ) - 1 );
+		if( getBoard( ).positionExists( p ) && isThereOpponentPiece( p ) ) {
+			mat[ p.getRow( ) ][ p.getColumn( ) ] = true;
+		}
 	}
 }
