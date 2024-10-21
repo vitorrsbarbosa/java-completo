fa14472db62750bc2d3512ef3b6f28113eb679cb
Vitor <vitorricardosilvabarbosa@gmail.com>
2024-10-21 13:53:13 -0300

    Understanding file reading #2

--- src/exercicio10/Program.java
+++ src/exercicio10/Program.java
@@ -1,23 +1,35 @@
 package exercicio10;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Program {
 	public static void main( String[] args ) {
-		File file = new File( "C:\\Temp\\in.txt" );
-		Scanner sc = null;
-		try {
-			sc = new Scanner( file );
-			while( sc.hasNextLine( ) ) {
-				System.out.println( sc.nextLine( ) );
+		String filePath = "C:\\Temp\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+		try{
+			fr = new FileReader(filePath);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			while(line != null){
+				System.out.println(line);
+				line = br.readLine();
 			}
+
 		} catch( IOException e ) {
-			System.err.println( "Erro: " + e.getMessage( ) );
+			System.err.println( "Error: " + e.getMessage() );
 		} finally {
-			assert sc != null;
-			sc.close( );
+			try {
+				if( br != null ) {
+					br.close( );
+				}
+				if( fr != null ) {
+					fr.close( );
+				}
+			} catch( IOException e ) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
