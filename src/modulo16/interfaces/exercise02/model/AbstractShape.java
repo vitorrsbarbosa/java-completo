e82d6ee79fd50af7b4df81ac55236fe31f2aa4be
Vitor <vitorricardosilvabarbosa@gmail.com>
2026-06-16 23:00:11 -0300

    Create getter, setter and constructor of abstract class

--- src/modulo16/interfaces/exercise02/model/AbstractShape.java
+++ src/modulo16/interfaces/exercise02/model/AbstractShape.java
@@ -5,4 +5,16 @@ import modulo16.interfaces.exercise02.services.Shape;
 
 public abstract class AbstractShape implements Shape {
 	private Color color;
+
+	public AbstractShape(Color color) {
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
 }
