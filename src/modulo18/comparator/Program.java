d1a6e5a51da470cde3fe0a6acd9f097a020923d6
Vitor <vitorricardosilvabarbosa@gmail.com>
2026-07-14 10:02:27 -0300

    Implementation of comparator method

--- src/modulo18/comparator/Program.java
+++ src/modulo18/comparator/Program.java
@@ -1,6 +1,7 @@
 package modulo18.comparator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import modulo18.comparator.entities.Product;
@@ -13,7 +14,11 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        // list.sort(arg0);
+        Comparator<Product> comparator = (p1, p2) -> {
+            return p1.getName().compareTo(p2.getName());
+        };
+
+        list.sort(comparator);
 
         for (Product p : list) {
             System.out.println(p.getName() + ", " + p.getPrice());
